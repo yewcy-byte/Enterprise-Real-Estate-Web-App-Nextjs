ffff23f1d34c2e52218b19dd1d9421e76420f225
Yew Choon Yuen <yewcyuen@gmail.com>
2026-05-18 01:17:34 +0800

    deployment update

---
+++ server/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.export= {
+    apps: [
+        {
+            name: "project-management",
+            script: "npm",
+            args: "run dev",
+            env: {
+                NODE_ENV: "development",
+               
+}}]
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -38,8 +38,8 @@ app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), ManagerRoutes);
 
 /*server*/
-const port = process.env.PORT || 3002;
-app.listen(port, () => {
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
 });
 
